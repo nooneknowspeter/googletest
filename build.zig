ac5f7c6a9c1b02ff5f6762edc2563250b12a2238
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-25 03:29:06 -0400

    feat: update build configuration to zig 0.15.1

--- build.zig
+++ build.zig
@@ -6,36 +6,29 @@ pub fn build(b: *std.Build) void {
 
     const googletest_dep = b.dependency("googletest", .{});
 
-    const gtest = b.addStaticLibrary(.{
-        .name = "gtest",
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-    gtest.linkLibCpp();
-    gtest.addCSourceFile(.{
-        .file = googletest_dep.path("googletest/src/gtest-all.cc"),
-        .flags = &.{},
-    });
-    gtest.addIncludePath(googletest_dep.path("googletest/include"));
-    gtest.addIncludePath(googletest_dep.path("googletest"));
+    const gtest = b.addLibrary(.{ .name = "gtest", .root_module = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true, .link_libcpp = true }) });
+
+    gtest.root_module.addCSourceFile(.{ .file = googletest_dep.path("googletest/src/gtest-all.cc"), .flags = &.{} });
+    gtest.root_module.addIncludePath(googletest_dep.path("googletest/include"));
+    gtest.root_module.addIncludePath(googletest_dep.path("googletest"));
     gtest.installHeadersDirectory(googletest_dep.path("googletest/include"), ".", .{});
 
     b.installArtifact(gtest);
 
-    const gtest_main = b.addStaticLibrary(.{
-        .name = "gtest_main",
+    const gtest_main = b.addLibrary(.{ .name = "gtest_main", .root_module = b.createModule(.{
         .target = target,
         .optimize = optimize,
         .link_libc = true,
-    });
-    gtest_main.linkLibCpp();
-    gtest_main.addCSourceFile(.{
+        .link_libcpp = true,
+    }) });
+
+    gtest_main.root_module.addCSourceFile(.{
         .file = googletest_dep.path("googletest/src/gtest_main.cc"),
         .flags = &.{},
     });
-    gtest_main.addIncludePath(googletest_dep.path("googletest/include"));
-    gtest_main.addIncludePath(googletest_dep.path("googletest"));
+    gtest_main.root_module.addIncludePath(googletest_dep.path("googletest/include"));
+    gtest_main.root_module.addIncludePath(googletest_dep.path("googletest"));
     gtest_main.installHeadersDirectory(googletest_dep.path("googletest/include"), ".", .{});
+
     b.installArtifact(gtest_main);
 }
